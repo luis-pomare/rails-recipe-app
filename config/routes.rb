cb3cfd1a7309d554c47c53ec8adb6d52b229a867
Luis Pomare <luispomare92@gmail.com>
2023-06-27 14:23:16 -0500

    Filter public recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :recipes
   resources :foods
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/public_recipes', to: 'public_recipes#index'
+  root "public_recipes#index"
 end
