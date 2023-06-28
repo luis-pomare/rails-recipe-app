573c1c30df2d8fd8fc17e23f083e534802df7fbc
Luis Pomare <luispomare92@gmail.com>
2023-06-28 10:59:21 -0500

    Create new ingredient route

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: %i[show edit update destroy]
+  before_action :set_recipe, only: %i[show edit update destroy add_ingredient]
 
   # GET /recipes or /recipes.json
   def index
@@ -53,6 +53,9 @@ class RecipesController < ApplicationController
       format.html { redirect_to recipes_url, notice: 'Recipe was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def add_ingredient
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :recipes
+  resources :recipes do
+    member do
+      get 'add_ingredient'
+    end
+  end
+
   resources :foods
   devise_for :users
+  
   get '/public_recipes', to: 'public_recipes#index'
   root "public_recipes#index"
 end
