f805281146446e771ebfdf2daeac5519699caad4
Luis Pomare <luispomare92@gmail.com>
2023-06-28 16:40:36 -0500

    Create recipe food form for a single atribute

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,4 +1,19 @@
 class RecipeFoodsController < ApplicationController
   def create
+    @recipe_food = RecipeFood.new(recipe_food_params)
+    @recipe_food.recipe = Recipe.first
+    @recipe_food.food = Food.first
+
+    respond_to do |format|
+      if @recipe_food.save
+        format.html { redirect_to recipe_url(Recipe.first), notice: 'Ingredient was added succesfully' }
+      end
+    end
+  end
+
+  protected
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity)
   end
 end
