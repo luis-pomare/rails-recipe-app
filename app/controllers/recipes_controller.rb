82d255d49cf4435d1ebd09e72b35f56d9a9bb618
Luis Pomare <luispomare92@gmail.com>
2023-06-29 15:43:49 -0500

    Create recipe ingredient list with basic info

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,7 +7,9 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1 or /recipes/1.json
-  def show; end
+  def show
+    @recipe = Recipe.includes(:recipe_food).find(params[:id])
+  end
 
   # GET /recipes/new
   def new
