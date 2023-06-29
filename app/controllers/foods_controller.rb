140fed8f93c7dfdf8d9df56eea65a294b53eda8c
Luis Pomare <luispomare92@gmail.com>
2023-06-29 17:06:09 -0500

    Add ingredient links

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -38,7 +38,7 @@ class FoodsController < ApplicationController
   def update
     respond_to do |format|
       if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully updated.' }
+        format.html { redirect_to request.referrer, notice: 'Food was successfully updated.' }
         format.json { render :show, status: :ok, location: @food }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -54,7 +54,7 @@ class FoodsController < ApplicationController
     @food.destroy
 
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+      format.html { redirect_to request.referrer, notice: 'Food was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
