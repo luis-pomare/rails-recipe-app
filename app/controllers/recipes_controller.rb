f9b1da985a0a1942cac5e3acf0f2364421503071
Luis Pomare <luispomare92@gmail.com>
2023-06-30 13:41:44 -0500

    Generate shopping list with no owned foods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: %i[show edit update destroy add_ingredient ]
+  before_action :set_recipe, only: %i[show edit update destroy add_ingredient shopping_list]
 
   # GET /recipes or /recipes.json
   def index
@@ -60,13 +60,13 @@ class RecipesController < ApplicationController
   end
 
   def add_ingredient
-    @foods = Food.where(user: current_user)
+    @foods = Food.all
     @new_food = Food.new
     @new_recipe_food = RecipeFood.new
   end
 
   def shopping_list
-    @recipe = Recipe.includes(:recipe_food).find(params[:id])
+    @recipe_foods = @recipe.recipe_food
     @user_foods = Food.where(user: current_user)
   end
 
