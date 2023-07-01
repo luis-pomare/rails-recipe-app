95b92312d41bb96724fc3c8e4de6b9eae3289154
Luis Pomare <luispomare92@gmail.com>
2023-07-01 11:14:28 -0500

    Fix linter errors

--- spec/integration/public_recipe_spec.rb
+++ spec/integration/public_recipe_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 RSpec.describe 'User', type: :system do
   describe 'Users' do
-    let(:recipe) { Recipe.create(name: 'cake', preparation_time: 20, cooking_time: 10, description: 'Cake recipe',  public: true, user_id: 1, id: 1) }
+    let(:recipe) do
+      Recipe.create(name: 'cake', preparation_time: 20, cooking_time: 10, description: 'Cake recipe', public: true,
+                    user_id: 1, id: 1)
+    end
 
     describe 'testing page rendering' do
       it 'should render the public recipe path' do
