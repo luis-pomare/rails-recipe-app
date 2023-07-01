893993ae161c9105cfc299e295fc4f3f3af1c097
SFarida <faridagueleu@gmail.com>
2023-07-01 08:16:19 +0100

    Allow access to the public recipes only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+  before_action :authorize_user, unless: :devise_controller?
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :surname, :email, :password, :current_password)
     end
+  end
+
+  def authorize_user
+    return if user_signed_in? || request.path == '/'
+
+    redirect_to new_user_session_path, alert: 'You are not authorized to access this page.'
   end
 end
