ce2cc1f05a604743f9c7fea312461468e744a32e
Luis Pomare <luispomare92@gmail.com>
2023-08-07 23:16:24 -0500

    Create seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user_one = User.create(name: user_one, email: 'user_one@gmail.com')
